7662e0619d5a03cec178e147649c2eb298993335
Khang Cao Hoàng <tinlun0386@gmail.com>
2022-11-02 23:23:50 +0700

    add hw lab3

---
+++ LAB3/homework1,2/src/pseudoAPI/drumstyleAPI.js
@@ -0,0 +1,29 @@
+
+const imgDir = '../assets/drum/images/'
+const mp3Dir = '../assets/drum/sounds/'
+
+export default function getDrumSylte() {
+  const keys = ['w', 'a', 's', 'd', 'j', 'k', 'l']
+
+  let styledata = []
+
+  keys.forEach((letter, index) => {
+    const item = {
+      id: index,
+      // style: {
+      //   backgroundImage: `url(${imgDir + letter}.png)`,
+      // },
+      // // clickHandler:a,
+
+      text: letter,
+      // className: 'drum-key ' + letter,
+      bgImg: `url(${imgDir + letter}.png)`,
+      sound: mp3Dir + `/${letter}.mp3`
+    }
+
+    styledata.push(item)
+  })
+
+  return styledata
+
+}
